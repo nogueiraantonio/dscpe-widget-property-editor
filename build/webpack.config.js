c53f626782c7bc478815eb74ecfa65472f49ded6
Romain Bouleis <romain.bouleis@3ds.com>
2019-08-07 17:16:14 +0200

    improve open in browser behavior

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -7,10 +7,12 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // set using
 // npm config set widget-template-vue:publicPath "https://3dexp.19xfd03.ds/WidgetLab/"
 let publicPath = process.env.npm_package_config_publicPath;
+let publicUrl = publicPath;
 let host = "0.0.0.0";
 if (publicPath === undefined || publicPath === null || publicPath.trim() === "") {
     publicPath = "";
     host = "localhost";
+    publicUrl = undefined;
 }
 
 module.exports = env => {
@@ -26,9 +28,11 @@ module.exports = env => {
             disableHostCheck: true,
             // host must be 0.0.0.0 if we want to be reachable from LAN
             host,
+            // if publicPath, open the browser on the right url
+            public: publicUrl,
             // to prevent CORS issues
             headers: { "Access-Control-Allow-Origin": "*" },
-            writeToDisk: true
+            writeToDisk: false
         },
         output: {
             filename: "bundle.js",
