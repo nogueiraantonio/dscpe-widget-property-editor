18dfa4b7f740cf3344c1229324657904856191a2
Romain Bouleis <romain.bouleis@3ds.com>
2019-08-08 14:50:32 +0200

    add support for md icons
    was because css for material icons was not loaded.
    it's now possible to use mdi (https://materialdesignicons.com/) and md (https://material.io/resources/icons) icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import "@mdi/font/css/materialdesignicons.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(Vuetify);
 
