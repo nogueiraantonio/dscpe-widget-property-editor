11fd27d5083035d1619586c7a2a75873087edc74
Romain Bouleis <romain.bouleis@3ds.com>
2019-08-16 15:01:41 +0200

    update for s3
    correct bug regarding S3 options which were not passed
    replace s3 npm API with newer fork compatible with signatures V4

--- build/webpack.config.dev.s3.js
+++ build/webpack.config.dev.s3.js
@@ -7,15 +7,15 @@ module.exports = merge(commonDev, {
     devServer: {},
     plugins: [
         new DevServerUploadToS3Plugin({
-            // uses s3. see https://www.npmjs.com/package/s3
+            // uses s3. see https://www.npmjs.com/package/@auth0/s3
 
             // any other options are passed to new AWS.S3()
             // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
             options: {
                 // aws sdk will use default profile if no accessKeyId & secretAccessKey are provided
-                /* accessKeyId: "xxx",
-                secretAccessKey: "xxx" */
-                region: "eu-west-1"
+                // accessKeyId: "xxx",
+                // secretAccessKey: "xxx"
+                // region: "eu-west-1"
             },
             // other options supported by putObject, except Body and ContentLength.
             // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
@@ -23,7 +23,7 @@ module.exports = merge(commonDev, {
                 Bucket: "btcc",
                 ACL: "public-read",
                 // distant path ;file path & name will be concatenated to the Key parameter
-                Key: "II2/WidgetLab/WidgetTemplate"
+                Key: "dist"
             }
         })
     ]
