ebdec4e7651e9f6760e00466f81b966ac42e0609
FEYEUX Julien <julien.feyeux@3ds.com>
2019-10-21 10:57:42 +0200

    Move back plugin declaration in S3 config

--- localConfig.template.js
+++ localConfig.template.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const DevServerUploadToS3Plugin = require("./webpack/plugin-dev-server-upload-to-s3");
 
 module.exports = {
     devServer: {
@@ -9,26 +8,18 @@ module.exports = {
             ca: fs.readFileSync("path/to/mkcert/files/rootCA.pem")
         }
     },
-    plugins: [
-        new DevServerUploadToS3Plugin({
-            // uses s3. see https://www.npmjs.com/package/@auth0/s3
-
-            // any other options are passed to new AWS.S3()
-            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-            options: {
-                // aws sdk will use default profile if no accessKeyId & secretAccessKey are provided
-                accessKeyId: "your_AWS_AccessKeyId",
-                secretAccessKey: "your_AWS_SecretAccessKey",
-                region: "your_AWS_S3_bucket_region"
-            },
-            // other options supported by putObject, except Body and ContentLength.
-            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-            params: {
-                Bucket: "your_bucket_name",
-                ACL: "public-read",
-                // distant path ;file path & name will be concatenated to the Key parameter
-                Key: "path/inside/bucket"
-            }
-        })
-    ]
+    s3: {
+        options: {
+            // aws sdk will use default profile if no accessKeyId & secretAccessKey are provided
+            accessKeyId: "your_AWS_AccessKeyId",
+            secretAccessKey: "your_AWS_SecretAccessKey",
+            region: "your_AWS_S3_bucket_region"
+        },
+        params: {
+            Bucket: "your_bucket_name",
+            ACL: "public-read",
+            // distant path ;file path & name will be concatenated to the Key parameter
+            Key: "path/inside/bucket"
+        }
+    }
 };
--- webpack/webpack.config.dev.s3.js
+++ webpack/webpack.config.dev.s3.js
@@ -1,5 +1,24 @@
 const merge = require("webpack-merge");
 const commonDev = require("./webpack.config.dev.js");
-const localConfigPlugins = require("../localConfig.js").plugins;
+const DevServerUploadToS3Plugin = require("./webpack-dev-server-s3");
+const localConfigS3Options = require("../localConfig.js").s3.options;
+const localConfigS3Params = require("../localConfig.js").s3.params;
 
-module.exports = merge(commonDev, { plugins: localConfigPlugins });
+module.exports = merge(commonDev, {
+    plugins: [
+        new DevServerUploadToS3Plugin({
+            // uses s3. see https://www.npmjs.com/package/@auth0/s3
+
+            // any other options are passed to new AWS.S3()
+            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+            options: {
+                localConfigS3Options
+            },
+            // other options supported by putObject, except Body and ContentLength.
+            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+            params: {
+                localConfigS3Params
+            }
+        })
+    ]
+});
