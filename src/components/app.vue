368ad24679e9e376df59170c525427cc035ff1f0
Romain Bouleis <romain.bouleis@3ds.com>
2020-08-19 13:37:49 +0200

    simplify vuetify template example

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,10 +1,47 @@
 <template>
-    <v-app>
-        <v-content>
-            <v-container class="fill-height">
-                <how-to />
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-home</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-email</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Contact</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app color="indigo" dark>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+
+        <v-main>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col class="text-center">
+                        <span>{{ message }}</span>
+                        <br />
+                        <v-btn class="ma-2" text icon color="pink lighten-1" @click="$store.commit('swapMessage')">
+                            <v-icon>mdi-heart</v-icon>
+                        </v-btn>
+                    </v-col>
+                </v-row>
             </v-container>
-        </v-content>
+        </v-main>
+        <v-footer color="indigo" app>
+            <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
     </v-app>
 </template>
 
@@ -13,31 +50,25 @@
 html,
 body {
     overflow-y: auto !important;
-    height: 100%;
+    overflow-x: hidden !important;
 }
 </style>
 
 <script>
-import HowTo from "./how-to.vue";
-
 // widget object is available everywhere !
 console.debug(widget);
 
 export default {
     name: "App",
-    components: { HowTo },
-    data: function() {
+    data() {
         return {
-            hello: "3DExperience Widget :)"
+            drawer: false
         };
     },
     computed: {
-        message: function() {
-            return this.$store.state.message;
+        message() {
+            return this.$store.getters.currentMessage;
         }
-    },
-    watch: {},
-    mounted: function() {},
-    methods: {}
+    }
 };
 </script>
